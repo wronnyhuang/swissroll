5487b217a914fbedd9f4c81d08ca0de7968fd74c
wrhuang <Ronny.Huang@ey.com>
2019-05-06 20:01:47 -0400

    success in reproducing old result. now trying to use sigopt to reoptmize for different noise seed

--- reproduce_clean_and_poison.sh
+++ reproduce_clean_and_poison.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python main.py -noise=1 -lr=.0038112 -lrstep2=6024 -lrstep=2781 -wdeccoef=.0012448 -distrfrac=0   -sugg=cleanwdec-opt -nepoch=20000 -gpu=1 -seed=1234 -save &
-python main.py -noise=1 -lr=.0067    -lrstep2=6452 -lrstep=3000 -wdeccoef=0        -distrfrac=.55 -sugg=poison-opt    -nepoch=20000 -gpu=1 -seed=1234 -save &
-python main.py -noise=1 -lr=.0038112 -lrstep2=6024 -lrstep=2781 -wdeccoef=0        -distrfrac=.75   -sugg=perfect       -nepoch=20000 -gpu=1 -seed=1234 -save -perfect &
+python main.py -noise=1 -lr=.0038112 -lrstep2=6024 -lrstep=2781 -wdeccoef=.0012448 -distrfrac=0   -sugg=clean_repro  -nepoch=20000 -gpu=1 -seed=1234 -save &
+python main.py -noise=1 -lr=.0067    -lrstep2=6452 -lrstep=3000 -wdeccoef=0        -distrfrac=.55 -sugg=poison_repro -nepoch=20000 -gpu=1 -seed=1234 -save &
+#python main.py -noise=1 -lr=.0038112 -lrstep2=6024 -lrstep=2781 -wdeccoef=0        -distrfrac=.75   -sugg=perfect       -nepoch=20000 -gpu=1 -seed=1234 -save -perfect &
