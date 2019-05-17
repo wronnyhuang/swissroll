b359926223c0b811589f19ce06fae0d631c368e1
wrhuang <Ronny.Huang@ey.com>
2019-05-17 00:37:45 -0400

    minor

--- generate_rollouts.py
+++ generate_rollouts.py
@@ -22,17 +22,23 @@ projname = 'poisonfrac-11'
 experiments = api.get('wronnyhuang/swissroll-' + projname)
 
 ## run rollouts
+f = open('script_generate_rollouts.sh', 'w+')
 processes = []
 for i, expt in enumerate(experiments):
 
-  while sum([process.poll() is None for process in processes]) > 60:
-    sleep(5)
+  # while sum([process.poll() is None for process in processes]) > 60:
+  #   sleep(5)
 
   gpu = i % 3
-  gpu += 1
+  # gpu += 1
   sugg = expt.name + '-rollout'
   tag = 'rollout-6'
+  pretrain_dir = join('ckpt/swissroll', projname, expt.name)
+  pretrain_url = get_dropbox_url(pretrain_dir, bin_path=bin_path)
   command = 'python main.py -seed=1237 -ndata=400 -noise=.5 -gpu=%s -rollout -nspan=601 -span=1 -nhidden 23 16 26 32 28 31 -sugg=%s -tag=%s -pretrain_dir=' % (gpu, sugg, tag) + \
               join('ckpt/swissroll', projname, expt.name)
-  processes.append(subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8'))
+  # processes.append(subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8'))
+  f.write(command + '\n')
+  
+f.close()
 
